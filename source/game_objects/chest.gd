0a933f3941bb10e42c2bee2f65928a96e062c228
Xanderath <alexgiller@proton.me>
2026-09-12 16:35:15 -0400

    added chest

---
+++ source/game_objects/chest.gd
@@ -0,0 +1,23 @@
+extends Node2D
+## Drops should be a child of the chest, be invisisble and have monitarable set to false
+
+var opened:=false
+@export var drop_radius_minimum:float=16
+@export var drop_radius_maximum:float=64
+@export_range(0,360,1,"radians_as_degrees") var drop_angle_minimum:float=0
+@export_range(0,360,1,"radians_as_degrees") var drop_angle_maximum:float=PI
+
+func open():
+	if not opened:
+		$Sprite2D.frame=1
+		for drop in get_children():
+			if not drop.is_in_group("not_dropped_from_chest"):
+				var drop_target=randf_range(drop_radius_minimum,drop_radius_maximum)*Vector2.from_angle(randf_range(drop_angle_minimum,drop_angle_maximum))
+				var tween=create_tween()
+				tween.set_ease(Tween.EASE_OUT)
+				tween.tween_property(drop,"position",drop_target,.5)
+				drop.show()
+				if drop is Area2D:
+					drop.set_deferred("monitorable",true)
+				if drop.has_method("_on_found_in_chest"):
+					drop._on_found_in_chest()
