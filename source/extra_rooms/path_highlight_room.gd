e91c8297ead78b0b5569ff88b2afcdc3ab54bdcf
Xanderath <alexgiller@proton.me>
2026-09-12 19:46:31 -0400

    made the path hilight room bigger, and have the camera zoom out when looking at the path

--- source/extra_rooms/path_highlight_room.gd
+++ source/extra_rooms/path_highlight_room.gd
@@ -7,8 +7,16 @@ func _ready():
 
 func highlight_path():
 	tween=create_tween()
+	tween.set_parallel()
 	tween.tween_property(%PathHighlights,"modulate",Color(1,1,1,.5),.5)
+	tween.tween_property(Globals.main.camera,"zoom",Vector2(1.5,1.5),.5)
+	tween.tween_property(Globals.main.camera,"offset",Vector2(0,-128),.5)
+	
+	
 
 func unhighlight_path():
 	tween=create_tween()
+	tween.set_parallel()
 	tween.tween_property(%PathHighlights,"modulate",Color(1,1,1,0),.5)
+	tween.tween_property(Globals.main.camera,"zoom",Vector2(2,2),.5)
+	tween.tween_property(Globals.main.camera,"offset",Vector2.ZERO,.5)
