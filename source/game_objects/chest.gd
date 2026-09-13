43b3b135af9f8175bda9998e35100e4cf68a468e
Avery <monkeyinabowl@gmail.com>
2026-09-12 21:15:49 -0400

    fixed money not being picked up when it drops on your head

--- source/game_objects/chest.gd
+++ source/game_objects/chest.gd
@@ -15,10 +15,19 @@ func open():
 			if not drop.is_in_group("not_dropped_from_chest"):
 				var drop_target=randf_range(drop_radius_minimum,drop_radius_maximum)*Vector2.from_angle(randf_range(drop_angle_minimum,drop_angle_maximum))
 				var tween=create_tween()
+				var tween2 = create_tween()
+				var tween3 = create_tween()
 				tween.set_ease(Tween.EASE_OUT)
 				tween.tween_property(drop,"position",drop_target,.5)
+			
+				tween2.tween_property(drop, "scale", (drop as Node2D).scale * 1.50, .25)
+				tween2.tween_property(drop, "scale", (drop as Node2D).scale, .25)
+				
 				if drop is Area2D:
 					tween.tween_property(drop, "monitorable", true, 0)
+					if drop is MoneyPickup:
+						print("doing this!")
+						tween3.tween_callback(drop.onAnimFinish).set_delay(0.5)
 				drop.show()
 				if drop.has_method("_on_found_in_chest"):
 					drop._on_found_in_chest()
--- source/pickups/money_pickup.gd
+++ source/pickups/money_pickup.gd
@@ -2,3 +2,27 @@ class_name MoneyPickup
 extends Pickup
 
 @export var value:=1000
+
+
+func onAnimFinish() -> void:
+	var space : PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+	var query : PhysicsShapeQueryParameters2D = PhysicsShapeQueryParameters2D.new()
+	var collisionShape : CollisionShape2D = $CollisionShape2D
+	
+	query.shape = collisionShape.shape
+	query.transform = collisionShape.global_transform
+	# Collision mask is 4
+	query.collision_mask = 0b00000000_00000000_00000000_00000010
+	query.collide_with_areas = true
+	query.collide_with_bodies = false
+	
+	var result = space.intersect_shape(query)
+	if result:
+		for dict : Dictionary in result:
+			print("i see you twin")
+			var collider : Area2D = dict.collider
+			collider.emit_signal("area_entered", self)
+	else:
+		print("i dont see you twin")
+		
+	
