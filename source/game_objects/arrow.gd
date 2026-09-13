ebba956575aee43137a13988faae0ff159b8f8a7
Avery <monkeyinabowl@gmail.com>
2026-09-12 23:08:06 -0400

    arrows have slight variation in their velocity vectors directions

--- source/game_objects/arrow.gd
+++ source/game_objects/arrow.gd
@@ -1,17 +1,24 @@
 extends Area2D
 
-@export var velocity:Vector2=Vector2(1000,0)
+@export var velocity_magnitude : float = 1000.0
 @export var time_limit:=1.0
+var velocity_dir : Vector2
 var age:=0.0
 
+func _ready() -> void:
+	velocity_dir = Vector2(1, 0 + randf_range(-0.075, 0.075))
+	velocity_dir = velocity_dir.normalized()
+
 func _physics_process(delta: float) -> void:
-	position+=velocity*delta
-	age+=delta
-	if age>time_limit:
+	position+=velocity_dir * velocity_magnitude * delta
+	age += delta
+	if age > time_limit:
 		queue_free()
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body : Node2D) -> void:
 	if age > 0.05:
-		queue_free()
+		velocity_magnitude = 0.0
+		var tween = create_tween()
+		tween.tween_property(self, "scale", Vector2(0.01, 0.01), 0.25)
 	pass # Replace with function body.
--- source/game_objects/arrow_spawner.gd
+++ source/game_objects/arrow_spawner.gd
@@ -3,7 +3,7 @@ extends Node2D
 @export var arrow_cooldown:=1.0
 @export var delay : float = 0.0
 @export var arrow_scene:PackedScene
-@export var arrow_velocity:=Vector2(1000,0)
+@export var arrow_velocity : float = 1000.0
 @export var arrow_lifetime:=1.0
 
 func _ready() -> void:
@@ -15,6 +15,6 @@ func shoot_arrow() -> void:
 	$CrossbowSprite.play("shoot")
 	$CrossbowSprite2.play("shoot")
 	var arrow=arrow_scene.instantiate()
-	arrow.velocity=arrow_velocity
+	arrow.velocity_magnitude = arrow_velocity
 	arrow.time_limit=arrow_lifetime
 	add_child(arrow)
